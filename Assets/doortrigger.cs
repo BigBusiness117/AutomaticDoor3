be0c75118351b0754a9dac2937d9c3a63c78ce8e
BigBusiness117 <67163482+BigBusiness117@users.noreply.github.com>
2021-02-04 11:13:42 -0800

    Initial commit

---
+++ Assets/doortrigger.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doortrigger : MonoBehaviour
+{
+    public GameObject door;
+    public float speed = 1f;
+    private bool doorOpen = false;
+    private bool doorExit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        doorOpen = true;
+        doorExit = false;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        doorExit = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(doorOpen == true)
+        {
+            door.transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime );
+            if(door.transform.position.y >= 9) 
+            {
+                doorOpen = false;
+            }
+        }
+        if(doorExit == true)
+        {
+            doorOpen = false;
+            door.transform.Translate(new Vector3(0, -speed, 0) * Time.deltaTime);
+            if (door.transform.position.y <= 4.5)
+            {
+                doorExit = false;
+            }
+        }
+        
+    }
+}
